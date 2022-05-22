b4dcf12bf1d21d5bc8fec16c7b8fa676c235251b
vKxni <Konradtoesch@icloud.com>
2022-05-22 16:44:08 +0200

    fix ts error

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,31 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 const Dashboard: NextPage = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const [userData, setUserData] = useState();
 
-    const [ userData, setUserData ] = useState();
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("userData") ?? "null");
+    if (data?.jwt) {
+      setUserData(data);
+    } else {
+      router.push("/");
+    }
+  }, []);
 
-    useEffect(()=>{
-        // @ts-ignore
-        //idk about typescript so this line gave me error so I suppressed it, u may fix it if u have idea about typescript
-        const data = JSON.parse(localStorage.getItem('userData'));
-        if(data?.jwt){
-            setUserData(data);
-        }else{
-            router.push("/");
-        }
-    }, [])
-
-    return (
-        <>
-            <div>
-                <h1>Dashboard</h1>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
